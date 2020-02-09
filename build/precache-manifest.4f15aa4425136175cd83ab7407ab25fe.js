5c64b582c4fc337d4209ed5504baf0b477e686af
eyalganor123 <eyalganor@gmail.com>
2020-02-09 11:18:24 +0200

    add modules

---
+++ build/precache-manifest.4f15aa4425136175cd83ab7407ab25fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dcb009204a70868f7f558b611559bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e71089d61ab24b4ece7",
+    "url": "/static/css/main.9fd403ca.chunk.css"
+  },
+  {
+    "revision": "55f99e63d596434483ff",
+    "url": "/static/js/2.fb499094.chunk.js"
+  },
+  {
+    "revision": "2e71089d61ab24b4ece7",
+    "url": "/static/js/main.d597712a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  }
+]);
